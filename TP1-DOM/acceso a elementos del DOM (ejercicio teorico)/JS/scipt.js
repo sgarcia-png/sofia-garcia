ddd3966da50f1da420a14ccceab419461e717496
sgarcia-png <sgarcia@obralapiedad.com.ar>
2026-05-04 08:31:40 -0300

    Add files via upload

---
+++ TP1-DOM/acceso a elementos del DOM (ejercicio teorico)/JS/scipt.js
@@ -0,0 +1,22 @@
+
+//b
+var resultado = document.querySelector("a");
+console.log( typeof resultado);
+//c
+var resultado = document.querySelectorAll("a")[2].innerHTML; 
+console.log(typeof resultado);
+//d
+var resultado = document.querySelector("a").length; 
+console.log(resultado);
+//e
+var resultado = document.getElementById("fotos").innerHTML; 
+console.log(typeof resultado);
+//f
+var resultado = document.querySelectorAll("a")[4];
+console.log(resultado);
+//g
+ var resultado = document.getElementById("menu");
+ console.log(typeof resultado);
+ //h
+ var resultado = document.getElementById("menu").className;
+ console.log(typeof resultado);
